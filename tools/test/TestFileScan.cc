24bf8e528b2d94710db2b91d380079307c1a939b
Owen O'Malley <omalley@apache.org>
2016-07-07 22:44:11 -0700

    ORC-82. Compilation fix for ubuntu 12.
    Signed-off-by: Owen O'Malley <omalley@apache.org>

--- tools/test/TestFileScan.cc
+++ tools/test/TestFileScan.cc
@@ -34,21 +34,65 @@ TEST (TestFileScan, testNominal) {
   EXPECT_EQ("Rows: 32768\nBatches: 33\n", output);
   EXPECT_EQ("", error);
 
-  EXPECT_EQ(0, runProgram({pgm, "-b", "256", file}, output, error));
+  EXPECT_EQ(0, runProgram({pgm, std::string("-b"), std::string("256"), file},
+                          output, error));
   EXPECT_EQ("Rows: 32768\nBatches: 131\n", output);
   EXPECT_EQ("", error);
 
-  EXPECT_EQ(0, runProgram({pgm, "-b256", file}, output, error));
+  EXPECT_EQ(0, runProgram({pgm, std::string("-b256"), file}, output, error));
   EXPECT_EQ("Rows: 32768\nBatches: 131\n", output);
   EXPECT_EQ("", error);
 
-  EXPECT_EQ(0, runProgram({pgm, "--batch", "256", file}, output, error));
+  EXPECT_EQ(0, runProgram({pgm, std::string("--batch"), std::string("256"),
+          file}, output, error));
   EXPECT_EQ("Rows: 32768\nBatches: 131\n", output);
   EXPECT_EQ("", error);
 
-  EXPECT_EQ(0, runProgram({pgm, "--batch=256", file}, output, error));
+  EXPECT_EQ(0, runProgram({pgm, std::string("--batch=256"), file},
+                          output, error));
   EXPECT_EQ("Rows: 32768\nBatches: 131\n", output);
   EXPECT_EQ("", error);
+}
+
+/**
+ * This function locates the goal substring in the input and removes
+ * everything before it.
+ * stripPrefix("abcdef", "cd") -> "cdef"
+ * stripPrefix("abcdef", "xx") -> "abcdef"
+ */
+std::string stripPrefix(const std::string& input, const std::string goal) {
+  size_t loc = input.find(goal);
+  if (loc == std::string::npos) {
+    return input;
+  } else {
+    return input.substr(loc);
+  }
+}
+
+std::string removeChars(const std::string &input,
+                        const std::string& chars) {
+  std::string result;
+  size_t prev = 0;
+  size_t pos = input.find_first_of(chars);
+  while (pos != std::string::npos) {
+    if (pos > prev) {
+      result += input.substr(prev, pos - prev);
+    }
+    prev = pos + 1;
+    pos = input.find_first_of(chars, prev);
+  }
+  pos = input.length();
+  if (pos > prev) {
+    result += input.substr(prev, pos - prev);
+  }
+  return result;
+}
+
+TEST (TestFileScan, testRemoveChars) {
+  EXPECT_EQ("abcdef", removeChars("abcdef", "xyz"));
+  EXPECT_EQ("bbbddd", removeChars("aaabbbcccddd", "ca"));
+  EXPECT_EQ("aaaccc", removeChars("aaabbbcccddd", "bd"));
+  EXPECT_EQ("abcde", removeChars("axbxcxdxe", "x"));
 }
 
 TEST (TestFileScan, testBadCommand) {
@@ -56,31 +100,38 @@ TEST (TestFileScan, testBadCommand) {
   const std::string file = findExample("TestOrcFile.testSeek.orc");
   std::string output;
   std::string error;
-  EXPECT_EQ(1, runProgram({pgm, file, "-b"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("-b")}, output, error));
   EXPECT_EQ("", output);
   EXPECT_EQ("orc-scan: option requires an argument -- b\n"
             "Usage: orc-scan [-h] [--help]\n"
-            "                [-b<size>] [--batch=<size>] <filename>\n", error);
+            "                [-b<size>] [--batch=<size>] <filename>\n",
+            removeChars(stripPrefix(error, "orc-scan: "),"'`"));
 
-  EXPECT_EQ(1, runProgram({pgm, file, "-b", "20x"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("-b"),
+          std::string("20x")}, output, error));
   EXPECT_EQ("", output);
   EXPECT_EQ("The --batch parameter requires an integer option.\n", error);
 
-  EXPECT_EQ(1, runProgram({pgm, file, "-b", "x30"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("-b"),
+          std::string("x30")}, output, error));
   EXPECT_EQ("", output);
   EXPECT_EQ("The --batch parameter requires an integer option.\n", error);
 
-  EXPECT_EQ(1, runProgram({pgm, file, "--batch"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("--batch")},
+                          output, error));
   EXPECT_EQ("", output);
-  EXPECT_EQ("orc-scan: option `--batch' requires an argument\n"
+  EXPECT_EQ("orc-scan: option --batch requires an argument\n"
             "Usage: orc-scan [-h] [--help]\n"
-            "                [-b<size>] [--batch=<size>] <filename>\n", error);
+            "                [-b<size>] [--batch=<size>] <filename>\n",
+            removeChars(stripPrefix(error, "orc-scan: "), "'`"));
 
-  EXPECT_EQ(1, runProgram({pgm, file, "--batch", "20x"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("--batch"),
+          std::string("20x")}, output, error));
   EXPECT_EQ("", output);
   EXPECT_EQ("The --batch parameter requires an integer option.\n", error);
 
-  EXPECT_EQ(1, runProgram({pgm, file, "--batch", "x30"}, output, error));
+  EXPECT_EQ(1, runProgram({pgm, file, std::string("--batch"),
+            std::string("x30")}, output, error));
   EXPECT_EQ("", output);
   EXPECT_EQ("The --batch parameter requires an integer option.\n", error);
 }
