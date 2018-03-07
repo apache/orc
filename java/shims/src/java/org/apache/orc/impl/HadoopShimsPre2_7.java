98fdba9a3ee6bcdba07047db979d7880708a7ad5
Owen O'Malley <omalley@apache.org>
2018-03-07 14:16:18 -0800

    ORC-318. Change KeyProvider API to separate createLocalKey from decryptLocalKey.
    Fixes #227
    
    Signed-off-by: Owen O'Malley <omalley@apache.org>

--- java/shims/src/java/org/apache/orc/impl/HadoopShimsPre2_7.java
+++ java/shims/src/java/org/apache/orc/impl/HadoopShimsPre2_7.java
@@ -20,7 +20,6 @@ package org.apache.orc.impl;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.crypto.key.KeyProviderCryptoExtension;
-import org.apache.hadoop.crypto.key.KeyProviderExtension;
 import org.apache.hadoop.crypto.key.KeyProviderFactory;
 import org.apache.hadoop.fs.FSDataInputStream;
 import org.apache.orc.EncryptionAlgorithm;
@@ -33,6 +32,7 @@ import java.io.OutputStream;
 import java.security.GeneralSecurityException;
 import java.security.Key;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Shims for versions of Hadoop less than 2.7.
@@ -70,20 +70,61 @@ public class HadoopShimsPre2_7 implements HadoopShims {
   }
 
   /**
-   * Shim implementation for Hadoop's KeyProvider API that lets applications get
-   * access to encryption keys.
+   * Shim implementation for ORC's KeyProvider API that uses Hadoop's
+   * KeyProvider API and implementations. Most users use a Hadoop or Ranger
+   * KMS and thus should use this default implementation.
+   *
+   * The main two methods of ORC's KeyProvider are createLocalKey and
+   * decryptLocalKey. These are very similar to Hadoop's
+   * <pre>
+   *   EncryptedKeyVersion generateEncryptedKey(String keyVersionName);
+   *   KeyVersion decryptEncryptedKey(EncryptedKeyVersion encrypted)
+   * </pre>
+   * but there are some important differences.
+   * <ul>
+   * <li>Hadoop's generateEncryptedKey doesn't return the decrypted key, so it
+   *     would require two round trips (generateEncryptedKey and then
+   *     decryptEncryptedKey)to the KMS.</li>
+   * <li>Hadoop's methods require storing both the IV and the encrypted key, so
+   *     for AES256, it is 48 random bytes.</li>
+   * </ul>
+   *
+   * However, since the encryption in the KMS is using AES/CTR we know that the
+   * flow is:
+   *
+   * <pre>
+   *   tmpKey = aes(masterKey, iv);
+   *   cypher = xor(tmpKey, plain);
+   * </pre>
+   *
+   * which means that encryption and decryption are symmetric. Therefore, if we
+   * use the KMS' decryptEncryptedKey, and feed in a random iv and the right
+   * number of 0's as the encrypted key, we get the right length of a tmpKey.
+   * Since it is symmetric, we can use it for both encryption and decryption
+   * and we only need to store the random iv. Since the iv is 16 bytes, it is
+   * only a third the size of the other solution, and only requires one trip to
+   * the KMS.
+   *
+   * So the flow looks like:
+   * <pre>
+   *   encryptedKey = securely random 16 bytes
+   *   --- on KMS ---
+   *   tmpKey0 = aes(masterKey, encryptedKey)
+   *   tmpKey1 = aes(masterKey, encryptedKey+1)
+   *   decryptedKey = xor(tmpKey0, 0) + xor(tmpKey1, 0)
+   * </pre>
+   *
+   * In the long term, we should probably fix Hadoop's generateEncryptedKey
+   * to either take the random key or pass it back.
    */
   static class KeyProviderImpl implements KeyProvider {
     private final org.apache.hadoop.crypto.key.KeyProvider provider;
+    private final Random random;
 
-    KeyProviderImpl(Configuration conf) throws IOException {
-      List<org.apache.hadoop.crypto.key.KeyProvider> result =
-          KeyProviderFactory.getProviders(conf);
-      if (result.size() != 1) {
-        throw new IllegalArgumentException("Can't get KeyProvider for ORC" +
-            " encryption. Got " + result.size() + " results.");
-      }
-      provider = result.get(0);
+    KeyProviderImpl(org.apache.hadoop.crypto.key.KeyProvider provider,
+                    Random random) {
+      this.provider = provider;
+      this.random = random;
     }
 
     @Override
@@ -93,92 +134,92 @@ public class HadoopShimsPre2_7 implements HadoopShims {
 
     @Override
     public KeyMetadata getCurrentKeyVersion(String keyName) throws IOException {
-      return new KeyMetadataImpl(keyName, provider.getMetadata(keyName));
+      org.apache.hadoop.crypto.key.KeyProvider.Metadata meta =
+          provider.getMetadata(keyName);
+      return new KeyMetadata(keyName, meta.getVersions() - 1,
+          findAlgorithm(meta));
     }
 
     @Override
-    public KeyMetadata getKeyVersion(String keyName, int version,
-                                     EncryptionAlgorithm algorithm) {
-      return new KeyMetadataImpl(keyName, version, algorithm);
-    }
-
-    @Override
-    public Key getLocalKey(KeyMetadata key, byte[] iv) throws IOException {
+    public LocalKey createLocalKey(KeyMetadata key) throws IOException {
       EncryptionAlgorithm algorithm = key.getAlgorithm();
-      KeyProviderCryptoExtension.EncryptedKeyVersion encryptedKey =
+      byte[] encryptedKey = new byte[algorithm.getIvLength()];
+      random.nextBytes(encryptedKey);
+      KeyProviderCryptoExtension.EncryptedKeyVersion param =
           KeyProviderCryptoExtension.EncryptedKeyVersion.createForDecryption(
-              key.getKeyName(), buildKeyVersionName(key), iv,
+              key.getKeyName(), buildKeyVersionName(key), encryptedKey,
+              algorithm.getZeroKey());
+      try {
+        KeyProviderCryptoExtension.KeyVersion decryptedKey =
+            ((KeyProviderCryptoExtension) provider)
+                .decryptEncryptedKey(param);
+        return new LocalKey(new SecretKeySpec(decryptedKey.getMaterial(),
+                                              algorithm.getAlgorithm()),
+                            encryptedKey);
+      } catch (GeneralSecurityException e) {
+        throw new IOException("Can't create local encryption key for " + key, e);
+      }
+    }
+
+    @Override
+    public Key decryptLocalKey(KeyMetadata key,
+                               byte[] encryptedKey) throws IOException {
+      EncryptionAlgorithm algorithm = key.getAlgorithm();
+      KeyProviderCryptoExtension.EncryptedKeyVersion param =
+          KeyProviderCryptoExtension.EncryptedKeyVersion.createForDecryption(
+              key.getKeyName(), buildKeyVersionName(key), encryptedKey,
               algorithm.getZeroKey());
       try {
         KeyProviderCryptoExtension.KeyVersion decrypted =
-            ((KeyProviderCryptoExtension.CryptoExtension) provider)
-                .decryptEncryptedKey(encryptedKey);
+            ((KeyProviderCryptoExtension) provider)
+                .decryptEncryptedKey(param);
         return new SecretKeySpec(decrypted.getMaterial(),
             algorithm.getAlgorithm());
       } catch (GeneralSecurityException e) {
-        throw new IOException("Problem decrypting key " + key.getKeyName(), e);
+        return null;
       }
     }
   }
 
-  static class KeyMetadataImpl implements KeyMetadata {
-    private final String keyName;
-    private final int version;
-    private final EncryptionAlgorithm algorithm;
-
-    KeyMetadataImpl(String keyName, KeyProviderExtension.Metadata metadata) {
-      this.keyName = keyName;
-      version = metadata.getVersions() - 1;
-      algorithm = findAlgorithm(metadata);
+  static KeyProvider createKeyProvider(Configuration conf,
+                                       Random random) throws IOException {
+    List<org.apache.hadoop.crypto.key.KeyProvider> result =
+        KeyProviderFactory.getProviders(conf);
+    if (result.size() == 0) {
+      LOG.info("Can't get KeyProvider for ORC encryption from" +
+          " hadoop.security.key.provider.path.");
+      return new HadoopShimsPre2_3.NullKeyProvider();
+    } else {
+      return new KeyProviderImpl(result.get(0), random);
     }
+  }
 
-    KeyMetadataImpl(String keyName, int version, EncryptionAlgorithm algorithm){
-      this.keyName = keyName;
-      this.version = version;
-      this.algorithm = algorithm;
-    }
-
-    @Override
-    public String getKeyName() {
-      return keyName;
-    }
-
-    @Override
-    public EncryptionAlgorithm getAlgorithm() {
-      return algorithm;
-    }
-
-    @Override
-    public int getVersion() {
-      return version;
-    }
-
-    /**
-     * Find the correct algorithm based on the key's metadata.
-     * @param meta the key's metadata
-     * @return the correct algorithm
-     */
-    static EncryptionAlgorithm findAlgorithm(KeyProviderCryptoExtension.Metadata meta) {
-      String cipher = meta.getCipher();
-      if (cipher.startsWith("AES/")) {
-        int bitLength = meta.getBitLength();
-        if (bitLength == 128) {
-          return EncryptionAlgorithm.AES_128;
-        } else {
-          if (bitLength != 256) {
-            LOG.info("ORC column encryption does not support " + bitLength +
-                " bit keys. Using 256 bits instead.");
-          }
-          return EncryptionAlgorithm.AES_256;
+  /**
+   * Find the correct algorithm based on the key's metadata.
+   * @param meta the key's metadata
+   * @return the correct algorithm
+   */
+  static EncryptionAlgorithm findAlgorithm(KeyProviderCryptoExtension.Metadata meta) {
+    String cipher = meta.getCipher();
+    if (cipher.startsWith("AES/")) {
+      int bitLength = meta.getBitLength();
+      if (bitLength == 128) {
+        return EncryptionAlgorithm.AES_128;
+      } else {
+        if (bitLength != 256) {
+          LOG.info("ORC column encryption does not support " + bitLength +
+              " bit keys. Using 256 bits instead.");
         }
+        return EncryptionAlgorithm.AES_256;
       }
-      throw new IllegalArgumentException("ORC column encryption only supports" +
-          " AES and not " + cipher);
     }
+    throw new IllegalArgumentException("ORC column encryption only supports" +
+        " AES and not " + cipher);
   }
 
   @Override
-  public KeyProvider getKeyProvider(Configuration conf) throws IOException {
-    return new KeyProviderImpl(conf);
+  public KeyProvider getKeyProvider(Configuration conf,
+                                    Random random) throws IOException {
+    return createKeyProvider(conf, random);
   }
 }
