0f17474e91ec6c84b4ad02385a8ce8a68c33e486
stiga-huang <huangquanlong@gmail.com>
2018-03-07 17:29:32 -0800

    remove redundant checks

--- java/core/src/java/org/apache/orc/OrcUtils.java
+++ java/core/src/java/org/apache/orc/OrcUtils.java
@@ -458,8 +458,7 @@ public class OrcUtils {
    */
   public static
         TypeDescription convertTypeFromProtobuf(List<OrcProto.Type> types,
-                                                int rootColumn)
-          throws FileFormatException {
+                                                int rootColumn) {
     OrcProto.Type type = types.get(rootColumn);
     switch (type.getKind()) {
       case BOOLEAN:
@@ -504,17 +503,9 @@ public class OrcUtils {
         return result;
       }
       case LIST:
-        if (type.getSubtypesCount() != 1) {
-          throw new FileFormatException("LIST type should contains exactly " +
-                  "one subtype but has " + type.getSubtypesCount());
-        }
         return TypeDescription.createList(
             convertTypeFromProtobuf(types, type.getSubtypes(0)));
       case MAP:
-        if (type.getSubtypesCount() != 2) {
-          throw new FileFormatException("MAP type should contains exactly " +
-                  "two subtypes but has " + type.getSubtypesCount());
-        }
         return TypeDescription.createMap(
             convertTypeFromProtobuf(types, type.getSubtypes(0)),
             convertTypeFromProtobuf(types, type.getSubtypes(1)));
