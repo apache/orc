06e591a92f34a5d63e2c741053ee05408fd4a444
Gang Wu <gang.w@alibaba-inc.com>
2018-03-24 22:22:55 +0800

    modify comment

--- c++/src/ColumnWriter.cc
+++ c++/src/ColumnWriter.cc
@@ -1194,7 +1194,7 @@ namespace orc {
     bool hasNull = false;
     for (uint64_t i = 0; i < numValues; ++i) {
       if (notNull == nullptr || notNull[i]) {
-        // TimestampVectorBatch stores data in UTC timezone
+        // TimestampVectorBatch already stores data in UTC
         int64_t millsUTC = secs[i] * 1000 + nanos[i] / 1000000;
         tsStats->increase(1);
         tsStats->update(millsUTC);
