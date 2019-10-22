bc575a3f7cac20001cb3afaed3522968c942d1e1
Owen O'Malley <omalley@apache.org>
2019-10-22 16:32:20 -0700

    Revert "ORC-546. Fix reading timestamps with duplicated millis within a second."
    This reverts commit 37245bbfb526c978118e2f1c1dded557a7f029fd.
    
    Signed-off-by: Owen O'Malley <omalley@apache.org>

--- java/core/src/java/org/apache/orc/impl/TreeReaderFactory.java
+++ java/core/src/java/org/apache/orc/impl/TreeReaderFactory.java
@@ -975,9 +975,9 @@ public class TreeReaderFactory {
 
       for (int i = 0; i < batchSize; i++) {
         if (result.noNulls || !result.isNull[i]) {
-          int newNanos = parseNanos(nanos.next());
+          final int newNanos = parseNanos(nanos.next());
           long millis = (data.next() + base_timestamp)
-              * WriterImpl.MILLIS_PER_SECOND;
+              * WriterImpl.MILLIS_PER_SECOND + newNanos / 1_000_000;
           if (millis < 0 && newNanos > 999_999) {
             millis -= WriterImpl.MILLIS_PER_SECOND;
           }
