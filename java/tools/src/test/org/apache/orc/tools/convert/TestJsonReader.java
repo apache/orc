d1a57f82379b4749e5dc7cbe8ae887fc30ec9120
X <x@y.z>
2021-01-19 13:38:15 +0800

    test expand includes

--- java/tools/src/test/org/apache/orc/tools/convert/TestJsonReader.java
+++ java/tools/src/test/org/apache/orc/tools/convert/TestJsonReader.java
@@ -33,7 +33,9 @@ import java.time.ZonedDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class TestJsonReader {
     @Test
