e688f23b58e68f87662f4dab0c4ba3d631042a12
Panos Garefalakis <pgaref@apache.org>
2021-03-23 13:34:52 +0000

    Removing unused import
    Change-Id: Ic3318f4a0869db831f2490d6070f2f4249e477d8

--- java/mapreduce/src/test/org/apache/orc/mapreduce/TestMapreduceOrcOutputFormat.java
+++ java/mapreduce/src/test/org/apache/orc/mapreduce/TestMapreduceOrcOutputFormat.java
@@ -46,7 +46,6 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
