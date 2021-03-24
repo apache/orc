4fb7e5ea6b281b66524924f3cb00bebfb6fd5fbe
Pavan Lanka <planka@apple.com>
2021-03-24 09:02:26 -0700

    Removed space after license

--- java/core/src/java/org/apache/orc/impl/reader/tree/PrimitiveBatchReader.java
+++ java/core/src/java/org/apache/orc/impl/reader/tree/PrimitiveBatchReader.java
@@ -15,7 +15,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package org.apache.orc.impl.reader.tree;
 
 import org.apache.hadoop.hive.ql.exec.vector.VectorizedRowBatch;
