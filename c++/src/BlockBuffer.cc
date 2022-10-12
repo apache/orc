8cf1047f9ace3799df12f24d2a5096b17a9a6ed0
coderex2522 <rex6817@gmail.com>
2022-10-12 09:41:40 +0800

    ORC-1280: [C++] Implement block-based buffer(Part I)
    This closes #1271

---
+++ c++/src/BlockBuffer.cc
@@ -0,0 +1,85 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "BlockBuffer.hh"
+
+#include <algorithm>
+
+namespace orc {
+
+  BlockBuffer::BlockBuffer(MemoryPool& pool, uint64_t _blockSize)
+      : memoryPool(pool),
+        currentSize(0),
+        currentCapacity(0),
+        blockSize(_blockSize) {
+    if (blockSize == 0) {
+      throw std::logic_error("Block size cannot be zero");
+    }
+    reserve(blockSize);
+  }
+
+  BlockBuffer::~BlockBuffer() {
+    for (size_t i = 0; i < blocks.size(); ++i) {
+      memoryPool.free(blocks[i]);
+    }
+    blocks.clear();
+    currentSize = currentCapacity = 0;
+  }
+
+  BlockBuffer::Block BlockBuffer::getBlock(uint64_t blockIndex) const {
+    if (blockIndex >= getBlockNumber()) {
+      throw std::out_of_range("Block index out of range");
+    }
+    return Block(blocks[blockIndex],
+                 std::min(currentSize - blockIndex * blockSize, blockSize));
+  }
+
+  BlockBuffer::Block BlockBuffer::getNextBlock() {
+    if (currentSize < currentCapacity) {
+      Block emptyBlock(
+          blocks[currentSize / blockSize] + currentSize % blockSize,
+          blockSize - currentSize % blockSize);
+      currentSize = (currentSize / blockSize + 1) * blockSize;
+      return emptyBlock;
+    } else {
+      resize(currentSize + blockSize);
+      return Block(blocks.back(), blockSize);
+    }
+  }
+
+  void BlockBuffer::resize(uint64_t size) {
+    reserve(size);
+    if (currentCapacity >= size) {
+      currentSize = size;
+    } else {
+      throw std::logic_error("Block buffer resize error");
+    }
+  }
+
+  void BlockBuffer::reserve(uint64_t newCapacity) {
+    while (currentCapacity < newCapacity) {
+      char* newBlockPtr = memoryPool.malloc(blockSize);
+      if (newBlockPtr != nullptr) {
+        blocks.push_back(newBlockPtr);
+        currentCapacity += blockSize;
+      } else {
+        break;
+      }
+    }
+  }
+} // namespace orc
---
+++ c++/test/TestBlockBuffer.cc
@@ -0,0 +1,81 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "BlockBuffer.hh"
+#include "orc/OrcFile.hh"
+#include "wrap/gtest-wrapper.h"
+
+namespace orc {
+
+  TEST(TestBlockBuffer, size_and_capacity) {
+    MemoryPool* pool = getDefaultPool();
+    BlockBuffer buffer(*pool, 1024);
+
+    // block buffer will preallocate one block during initialization
+    EXPECT_EQ(buffer.getBlockNumber(), 0);
+    EXPECT_EQ(buffer.size(), 0);
+    EXPECT_EQ(buffer.capacity(), 1024);
+
+    buffer.reserve(128 * 1024);
+    EXPECT_EQ(buffer.getBlockNumber(), 0);
+    EXPECT_EQ(buffer.size(), 0);
+    EXPECT_EQ(buffer.capacity(), 128 * 1024);
+
+    // new size < old capacity
+    buffer.resize(64 * 1024);
+    EXPECT_EQ(buffer.getBlockNumber(), 64);
+    EXPECT_EQ(buffer.size(), 64 * 1024);
+    EXPECT_EQ(buffer.capacity(), 128 * 1024);
+
+    // new size > old capacity
+    buffer.resize(256 * 1024);
+    EXPECT_EQ(buffer.getBlockNumber(), 256);
+    EXPECT_EQ(buffer.size(), 256 * 1024);
+    EXPECT_EQ(buffer.capacity(), 256 * 1024);
+  }
+
+  TEST(TestBlockBuffer, get_block) {
+    MemoryPool* pool = getDefaultPool();
+    BlockBuffer buffer(*pool, 1024);
+
+    EXPECT_EQ(buffer.getBlockNumber(), 0);
+    for (uint64_t i = 0; i < 10; ++i) {
+      BlockBuffer::Block block = buffer.getNextBlock();
+      EXPECT_EQ(buffer.getBlockNumber(), i + 1);
+      for (uint64_t j = 0; j < block.size; ++j) {
+        if (i % 2 == 0) {
+          block.data[j] = static_cast<char>('A' + (i + j) % 26);
+        } else {
+          block.data[j] = static_cast<char>('a' + (i + j) % 26);
+        }
+      }
+    }
+
+    // verify the block data
+    for (uint64_t i = 0; i < buffer.getBlockNumber(); ++i) {
+      BlockBuffer::Block block = buffer.getBlock(i);
+      for (uint64_t j = 0; j < block.size; ++j) {
+        if (i % 2 == 0) {
+          EXPECT_EQ(block.data[j], 'A' + (i + j) % 26);
+        } else {
+          EXPECT_EQ(block.data[j], 'a' + (i + j) % 26);
+        }
+      }
+    }
+  }
+}
