cc77861e2bab1358f1aa98796338799cf105d581
coderex2522 <rex6817@gmail.com>
2022-10-25 14:07:36 +0800

    ORC-1286: [C++] fix the format

--- c++/src/BlockBuffer.hh
+++ c++/src/BlockBuffer.hh
@@ -66,6 +66,8 @@ namespace orc {
 
       Block() : data(nullptr), size(0) {}
       Block(char* _data, uint64_t _size) : data(_data), size(_size) {}
+      Block(const Block& block) = default;
+      ~Block() = default;
     };
 
     /**
