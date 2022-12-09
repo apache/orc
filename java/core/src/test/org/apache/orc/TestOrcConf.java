8701f49455784c2013f8170abd688ad6c07311b2
Deshan Xiao <deshanxiao@microsoft.com>
2022-12-09 06:00:58 -0800

    add test

---
+++ java/core/src/test/org/apache/orc/TestOrcConf.java
@@ -0,0 +1,125 @@
+package org.apache.orc;
+
+import org.apache.hadoop.conf.Configuration;
+import org.junit.jupiter.api.Test;
+
+import java.util.Properties;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class TestOrcConf {
+
+    // Long
+    public final OrcConf ORC_STRIPE_SIZE_CONF = OrcConf.STRIPE_SIZE;
+    // Boolean
+    public final OrcConf ORC_ENABLE_INDEXES_CONF = OrcConf.ENABLE_INDEXES;
+    // int
+    public final OrcConf ORC_BLOCK_SIZE_CONF = OrcConf.ENABLE_INDEXES;
+    // String
+    public final OrcConf ORC_COMPRESS_CONF = OrcConf.COMPRESS;
+    // Double
+    public final OrcConf ORC_BLOOM_FILTER_FPP_CONF = OrcConf.BLOOM_FILTER_FPP;
+
+    @Test
+    public void testLoadConfFromProperties() {
+        Properties tableProperties = new Properties();
+        Configuration conf = new Configuration();
+
+        tableProperties.setProperty(ORC_STRIPE_SIZE_CONF.getAttribute(), "1000");
+        conf.setInt(ORC_STRIPE_SIZE_CONF.getAttribute(), 2000);
+        assertEquals(1000, ORC_STRIPE_SIZE_CONF.getLong(tableProperties, conf));
+
+        tableProperties.setProperty(ORC_BLOCK_SIZE_CONF.getAttribute(), "1000");
+        conf.setInt(ORC_BLOCK_SIZE_CONF.getAttribute(), 2000);
+        assertEquals(1000, ORC_BLOCK_SIZE_CONF.getInt(tableProperties, conf));
+
+        tableProperties.setProperty(ORC_ENABLE_INDEXES_CONF.getAttribute(), "true");
+        conf.setBoolean(ORC_ENABLE_INDEXES_CONF.getAttribute(), false);
+        assertTrue(ORC_ENABLE_INDEXES_CONF.getBoolean(tableProperties, conf));
+
+        tableProperties.setProperty(ORC_COMPRESS_CONF.getAttribute(), "snappy2");
+        conf.set(ORC_COMPRESS_CONF.getAttribute(), "snappy3");
+        assertEquals("snappy2", ORC_COMPRESS_CONF.getString(tableProperties, conf));
+
+        tableProperties.setProperty(ORC_BLOOM_FILTER_FPP_CONF.getAttribute(), "0.5");
+        conf.setDouble(ORC_BLOOM_FILTER_FPP_CONF.getAttribute(), 0.4);
+        assertEquals(0.5, ORC_BLOOM_FILTER_FPP_CONF.getDouble(tableProperties, conf));
+    }
+
+    @Test
+    public void testLoadConfFromConfiguration() {
+        Properties tableProperties = new Properties();
+        Configuration conf = new Configuration();
+
+        conf.setInt(ORC_STRIPE_SIZE_CONF.getAttribute(), 2000);
+        assertEquals(2000, ORC_STRIPE_SIZE_CONF.getLong(tableProperties, conf));
+
+        conf.setInt(ORC_BLOCK_SIZE_CONF.getAttribute(), 2000);
+        assertEquals(2000, ORC_BLOCK_SIZE_CONF.getInt(tableProperties, conf));
+
+        conf.setBoolean(ORC_ENABLE_INDEXES_CONF.getAttribute(), false);
+        assertFalse(ORC_ENABLE_INDEXES_CONF.getBoolean(tableProperties, conf));
+
+        conf.set(ORC_COMPRESS_CONF.getAttribute(), "snappy3");
+        assertEquals("snappy3", ORC_COMPRESS_CONF.getString(tableProperties, conf));
+
+        conf.setDouble(ORC_BLOOM_FILTER_FPP_CONF.getAttribute(), 0.4);
+        assertEquals(0.4, ORC_BLOOM_FILTER_FPP_CONF.getDouble(tableProperties, conf));
+    }
+
+    @Test
+    public void testDefaultValue() {
+        Properties tableProperties = new Properties();
+        Configuration conf = new Configuration();
+
+        for (OrcConf orcConf: OrcConf.values()) {
+            if (orcConf.getDefaultValue() instanceof String) {
+                assertEquals(orcConf.getString(tableProperties, conf), orcConf.getDefaultValue());
+            }
+            if (orcConf.getDefaultValue() instanceof Long) {
+                assertEquals(orcConf.getLong(tableProperties, conf), orcConf.getDefaultValue());
+            }
+            if (orcConf.getDefaultValue() instanceof Integer) {
+                assertEquals(orcConf.getInt(tableProperties, conf), orcConf.getDefaultValue());
+            }
+            if (orcConf.getDefaultValue() instanceof Boolean) {
+                assertEquals(orcConf.getBoolean(tableProperties, conf), orcConf.getDefaultValue());
+            }
+            if (orcConf.getDefaultValue() instanceof Double) {
+                assertEquals(orcConf.getDouble(tableProperties, conf), orcConf.getDefaultValue());
+            }
+        }
+    }
+
+    @Test
+    public void testSetValue() {
+        Properties tableProperties = new Properties();
+        Configuration conf = new Configuration();
+
+        ORC_STRIPE_SIZE_CONF.setLong(conf, 1000);
+        assertEquals(1000, ORC_STRIPE_SIZE_CONF.getLong(tableProperties, conf));
+
+        ORC_BLOCK_SIZE_CONF.setInt(conf, 2000);
+        assertEquals(2000, ORC_BLOCK_SIZE_CONF.getLong(tableProperties, conf));
+
+        ORC_ENABLE_INDEXES_CONF.setBoolean(conf, false);
+        assertFalse(ORC_ENABLE_INDEXES_CONF.getBoolean(tableProperties, conf));
+
+        ORC_COMPRESS_CONF.setString(conf, "snappy3");
+        assertEquals("snappy3", ORC_COMPRESS_CONF.getString(tableProperties, conf));
+
+        ORC_COMPRESS_CONF.setDouble(conf, 0.4);
+        assertEquals(0.4, ORC_COMPRESS_CONF.getDouble(tableProperties, conf));
+    }
+
+    @Test
+    public void testGetHiveConfValue() {
+        Properties tableProperties = new Properties();
+        Configuration conf = new Configuration();
+
+        conf.setInt(ORC_STRIPE_SIZE_CONF.getHiveConfName(), 2000);
+        assertEquals(2000, ORC_STRIPE_SIZE_CONF.getLong(tableProperties, conf));
+    }
+}
