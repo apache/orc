f76c698907136c66c4143a18478ae39ea1342ec1
deshanxiao <deshanxiao@microsoft.com>
2023-02-07 16:58:58 +0800

    small fix

--- java/core/src/test/org/apache/orc/impl/TestWriterImpl.java
+++ java/core/src/test/org/apache/orc/impl/TestWriterImpl.java
@@ -32,12 +32,13 @@ import org.apache.orc.Writer;
 import org.apache.orc.*;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestWriterImpl {
 
