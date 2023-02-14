eeafccfe6973f8c3d1bef6e65b0ef468c2071a68
Your Name <you@example.com>
2023-02-13 23:33:24 -0500

    Fix some comments

--- c++/src/Bpacking.cc
+++ c++/src/Bpacking.cc
@@ -60,7 +60,6 @@ namespace orc {
   }
 
 #if defined(ORC_HAVE_RUNTIME_AVX512)
-  // template <typename UnpackSimd>
   int readLongsAvx512(RleDecoderV2* decoder, int64_t* data, uint64_t offset, uint64_t len,
                       uint64_t fbs) {
     UnpackAvx512 unpackAvx512(decoder);
