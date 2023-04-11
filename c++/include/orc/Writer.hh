b01addec7d8bbee77ec923a754481e962bb2cd66
Hinko Kocevar <hinko.kocevar@ess.eu>
2023-04-11 14:26:20 +0200

    use fixed-length data type

--- c++/include/orc/Writer.hh
+++ c++/include/orc/Writer.hh
@@ -309,7 +309,7 @@ namespace orc {
      * truncated to the returned offset, it would be a valid ORC file.
      * @return the offset that would be a valid end location for an ORC file
      */
-    virtual long writeIntermediateFooter() = 0;
+    virtual int64_t writeIntermediateFooter() = 0;
   };
 }  // namespace orc
 
