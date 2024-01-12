0292ebe101a65d0d95a585fa8bc6324c9b24ce56
sychen <sychen@ctrip.com>
2024-01-12 18:23:26 +0800

    java16 instanceOf

--- java/core/src/java/org/apache/orc/impl/PhysicalFsWriter.java
+++ java/core/src/java/org/apache/orc/impl/PhysicalFsWriter.java
@@ -118,10 +118,11 @@ public class PhysicalFsWriter implements PhysicalWriter {
       // HACK: Fetch Zstd-specific options and set them here -- need to find a
       // better way before merging.
       CompressionCodec.Options tempOptions = codec.getDefaultOptions();
-      if (codec instanceof ZstdCodec) {
-        ((ZstdCodec.ZstdOptions) tempOptions).setLevel(opts.getCompressionZstdLevel());
-        ((ZstdCodec.ZstdOptions) tempOptions).setWindowLog(opts.getCompressionZstdWindowLog());
-        ((ZstdCodec.ZstdOptions) tempOptions).setLongMode(opts.getCompressionZstdLongMode());
+      if (codec instanceof ZstdCodec &&
+              codec.getDefaultOptions() instanceof ZstdCodec.ZstdOptions options) {
+        options.setLevel(opts.getCompressionZstdLevel());
+        options.setWindowLog(opts.getCompressionZstdWindowLog());
+        options.setLongMode(opts.getCompressionZstdLongMode());
       }
       compress.withCodec(codec, tempOptions);
     }
