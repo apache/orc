0c6f3e70a51485547a27e8838fbf86f373d5e3cd
Gang Wu <ustcwg@gmail.com>
2024-02-28 09:20:15 +0800

    ORC-1637: [C++] Port conan recipe from upstream conan center
    ### What changes were proposed in this pull request?
    Port conan recipe back from the conan center index.
    
    ### Why are the changes needed?
    To keep it in sync with the upstream.
    
    ### How was this patch tested?
    Tested it locally. This has been tested by the CIs of conan center index.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    No.
    
    Closes #1824 from wgtmac/conan-center.
    
    Authored-by: Gang Wu <ustcwg@gmail.com>
    Signed-off-by: Gang Wu <ustcwg@gmail.com>

---
+++ conan/all/test_package/conanfile.py
@@ -0,0 +1,43 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import os
+
+from conan import ConanFile
+from conan.tools.cmake import CMake, cmake_layout
+from conan.tools.build import can_run
+
+class TestPackageConan(ConanFile):
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "CMakeDeps", "CMakeToolchain", "VirtualRunEnv"
+    test_type = "explicit"
+
+    def requirements(self):
+        self.requires(self.tested_reference_str)
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def test(self):
+        if can_run(self):
+            cmd = os.path.join(self.cpp.build.bindir, "test_package")
+            self.run(cmd, env="conanrun")
