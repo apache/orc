f202c2f011862ab7c791bcd2d16008c47b67233f
sychen <sychen@ctrip.com>
2024-04-01 10:46:01 +0800

    remove

--- java/tools/src/test/org/apache/orc/tools/TestCheckTool.java
+++ java/tools/src/test/org/apache/orc/tools/TestCheckTool.java
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.orc.tools.check;
+package org.apache.orc.tools;
 
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
