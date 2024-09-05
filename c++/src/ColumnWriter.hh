c9eb0e59bbb5867f34d4362a60c24440e4a68dc4
Hao Zou <34559830+luffy-zh@users.noreply.github.com>
2024-09-05 10:21:35 +0800

    Update c++/src/ColumnWriter.hh
    Co-authored-by: Gang Wu <ustcwg@gmail.com>

--- c++/src/ColumnWriter.hh
+++ c++/src/ColumnWriter.hh
@@ -183,8 +183,11 @@ namespace orc {
      * Finalize the encoding and compressing process. This function should be
      * called after all data required for encoding has been added. It ensures
      * that any remaining data is processed and the final state of the streams
-     * is set. Note: the boolean type may break this spec due to some trailing bits will be written
-     * to the next compression block.
+     * is set. 
+     * Note: boolean type cannot cut off the current byte if it is not filled
+     * with 8 bits, otherwise Boolean RLE may incorrectly read the unfilled 
+     * trailing bits. In this case, the last byte will be the head of the next
+     * compression block.
      */
     virtual void finishStreams();
 
