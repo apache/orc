78700f3d55f4389265e85eaf432dc3510822d35f
shuai-xu <xushuai@bigo.sg>
2024-10-25 17:23:22 +0800

    ORC-2054: [C++] fix return wrong result if lack of hasnull

--- c++/src/sargs/PredicateLeaf.cc
+++ c++/src/sargs/PredicateLeaf.cc
@@ -701,6 +701,10 @@ namespace orc {
       }
     }
 
+    // files written by trino may lack of hasnull field.
+    if (!colStats.has_hasnull())
+      return TruthValue::YES_NO_NULL;
+
     bool allNull = colStats.has_null() && colStats.number_of_values() == 0;
     if (operator_ == Operator::IS_NULL ||
         ((operator_ == Operator::EQUALS || operator_ == Operator::NULL_SAFE_EQUALS) &&
