658ef493712df0edb6c6aba31a76519807a05f1e
Deshan Xiao <deshanxiao@microsoft.com>
2024-11-28 10:54:35 +0800

    fix comments

---
+++ conan/all/test_package/CMakeFiles/test_package.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/test_package.dir/test_package.cpp.o"
+  "CMakeFiles/test_package.dir/test_package.cpp.o.d"
+  "test_package"
+  "test_package.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_package.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
