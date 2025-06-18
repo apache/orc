4aa3ff77f8d8687cc890218a28f8a3b3be2922a3
Dongjoon Hyun <dongjoon@apache.org>
2025-06-17 17:50:34 -0700

    ORC-1927: Add Java `25-ea` test coverage for `tools` modules

--- java/tools/src/java/org/apache/orc/tools/ColumnSizes.java
+++ java/tools/src/java/org/apache/orc/tools/ColumnSizes.java
@@ -216,7 +216,9 @@ public class ColumnSizes {
   }
 
   public static void main(String[] args) throws Exception {
-    main(new Configuration(), args);
+    Configuration conf = new Configuration();
+    conf.set("fs.file.impl.disable.cache", "true");
+    main(conf, args);
   }
 
   private static Options createOptions() {
