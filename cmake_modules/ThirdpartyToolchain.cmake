680af6c781268007a7d6240a7884444487e52277
Dongjoon Hyun <dongjoon@apache.org>
2025-09-12 14:32:56 -0700

    ORC-1893: [C++] Upgrade `zstd` to 1.5.7
    ### What changes were proposed in this pull request?
    
    This PR aims to upgrade `zstd` to 1.5.7 in `cmake_modules` module.
    
    `GitHub Action MacOS 15` is updated to ZSTD 1.5.7.
    
    - https://github.com/actions/runner-images/blob/macos-15-arm64/20250911.2324/images/macos/macos-15-arm64-Readme.md
    
    ### Why are the changes needed?
    
    To bring the latest bug fixes,
    - https://github.com/facebook/zstd/releases/tag/v1.5.6
    - https://github.com/facebook/zstd/releases/tag/v1.5.7
    
    ### How was this patch tested?
    
    Pass the CIs.
    
    ### Was this patch authored or co-authored using generative AI tooling?
    
    No.
    
    Closes #2396 from dongjoon-hyun/ORC-1893-2.0.
    
    Authored-by: Dongjoon Hyun <dongjoon@apache.org>
    Signed-off-by: Dongjoon Hyun <dongjoon@apache.org>

--- cmake_modules/ThirdpartyToolchain.cmake
+++ cmake_modules/ThirdpartyToolchain.cmake
@@ -21,7 +21,7 @@ set(SNAPPY_VERSION "1.2.2")
 set(ZLIB_VERSION "1.3.1")
 set(GTEST_VERSION "1.12.1")
 set(PROTOBUF_VERSION "3.5.1")
-set(ZSTD_VERSION "1.5.5")
+set(ZSTD_VERSION "1.5.7")
 
 option(ORC_PREFER_STATIC_PROTOBUF "Prefer static protobuf library, if available" ON)
 option(ORC_PREFER_STATIC_SNAPPY   "Prefer static snappy library, if available"   ON)
