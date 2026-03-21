568f62c09815ec3561655509f52622dfefe4912e
Dongjoon Hyun <dongjoon@apache.org>
2026-03-20 22:55:34 -0700

    Revert "ORC-2123: [C++] Fix heap-use-after-free in ORC SearchArgument rewriteLeaves"
    This reverts commit a81eaa54a6a86d8d232aa17df19b415b4a29c928.

--- c++/src/sargs/SearchArgument.cc
+++ c++/src/sargs/SearchArgument.cc
@@ -315,6 +315,7 @@ namespace orc {
     // Perform BFS
     while (!nodes.empty()) {
       TreeNode& node = nodes.front();
+      nodes.pop_front();
 
       if (node->getOperator() == ExpressionTree::Operator::LEAF) {
         leaves.insert(node);
@@ -323,7 +324,6 @@ namespace orc {
           nodes.push_back(child);
         }
       }
-      nodes.pop_front();
     }
 
     // Update the leaf in place
